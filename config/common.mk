2ed7f1cf62197ca009ae7c4e049cc4b0422d0076
Jens Reidel <adrian@travitia.xyz>
2022-12-27 19:24:17 +0100

    kenvyra: Drop partner GMS config
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- config/common.mk
+++ config/common.mk
@@ -228,4 +228,3 @@ include vendor/kenvyra/config/version.mk
 -include vendor/kenvyra-priv/keys/keys.mk
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
--include vendor/kenvyra/config/partner_gms.mk
