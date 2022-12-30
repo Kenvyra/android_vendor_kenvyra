eda62e49b1f6f46d43637c3eebed5371a4b092b9
Jens Reidel <adrian@travitia.xyz>
2022-12-30 02:29:27 +0100

    kenvyra: Rename ro.lineage.* props to ro.kenvyra.*
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -6,20 +6,20 @@ endif
 
 # LineageOS System Version
 ADDITIONAL_SYSTEM_PROPERTIES += \
-    ro.lineage.version=$(KENVYRA_VERSION) \
-    ro.lineage.releasetype=$(KENVYRA_BUILDTYPE) \
-    ro.lineage.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
+    ro.kenvyra.version=$(KENVYRA_VERSION) \
+    ro.kenvyra.releasetype=$(KENVYRA_BUILDTYPE) \
+    ro.kenvyra.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
     ro.modversion=$(KENVYRA_VERSION) \
-    ro.lineagelegal.url=https://lineageos.org/legal
+    ro.kenvyralegal.url=https://lineageos.org/legal
 
-# LineageOS Platform Display Version
+# Kenvyra Platform Display Version
 ADDITIONAL_SYSTEM_PROPERTIES += \
-    ro.lineage.display.version=$(KENVYRA_DISPLAY_VERSION)
+    ro.kenvyra.display.version=$(KENVYRA_DISPLAY_VERSION)
 
 # LineageOS Platform SDK Version
 ADDITIONAL_SYSTEM_PROPERTIES += \
-    ro.lineage.build.version.plat.sdk=$(LINEAGE_PLATFORM_SDK_VERSION)
+    ro.kenvyra.build.version.plat.sdk=$(LINEAGE_PLATFORM_SDK_VERSION)
 
 # LineageOS Platform Internal Version
 ADDITIONAL_SYSTEM_PROPERTIES += \
-    ro.lineage.build.version.plat.rev=$(LINEAGE_PLATFORM_REV)
+    ro.kenvyra.build.version.plat.rev=$(LINEAGE_PLATFORM_REV)
