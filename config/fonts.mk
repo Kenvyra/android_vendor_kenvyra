4abe6718ad5fa370a38dd81d49c01b46dfd92ddb
Jens Reidel <adrian@travitia.xyz>
2023-03-08 00:18:59 +0100

    fonts: Move Rubik and Lato inclusion logic to fonts.mk
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- config/fonts.mk
+++ config/fonts.mk
@@ -1,6 +1,17 @@
-# Fonts
+# Include {Lato,Rubik} fonts
+$(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
+$(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
+
+PRODUCT_PACKAGES += \
+    FontLatoOverlay \
+    FontRubikOverlay
+
+# Include Google Sans fonts
 PRODUCT_COPY_FILES += \
     vendor/kenvyra/fonts/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular.ttf \
     vendor/kenvyra/fonts/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic.ttf \
-    vendor/kenvyra/fonts/GoogleSansClock-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSansClock-Regular.ttf \
+    vendor/kenvyra/fonts/GoogleSansClock-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSansClock-Regular.ttf
+
+# Include fonts customization config
+PRODUCT_COPY_FILES += \
     vendor/kenvyra/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
