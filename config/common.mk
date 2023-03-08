b3e4ef73d840ed2ee69ee952e476c99caf40fb7a
Jens Reidel <adrian@travitia.xyz>
2023-03-08 16:17:06 +0100

    Revert "vendor: Import Pixel audio files"
    This reverts commit 76c08cfacd68e902d7a6628117eae32f01934b8e.

--- config/common.mk
+++ config/common.mk
@@ -105,18 +105,6 @@ OVERRIDE_TARGET_FLATTEN_APEX := true
 # prebuilt vendors, as init reads /product/build.prop after /vendor/build.prop
 PRODUCT_PRODUCT_PROPERTIES += ro.apex.updatable=false
 
-# Include Pixel audio files
-include vendor/kenvyra/config/PixelAudio.mk
-
-# Default notification/alarm sounds
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.config.notification_sound=Eureka.ogg \
-    ro.config.alarm_alert=Fresh_start.ogg
-
-# Default ringtone
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.config.ringtone=Your_new_adventure.ogg
-
 # Google Photos Pixel Exclusive XML
 PRODUCT_COPY_FILES += \
     vendor/kenvyra/prebuilt/common/etc/sysconfig/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
